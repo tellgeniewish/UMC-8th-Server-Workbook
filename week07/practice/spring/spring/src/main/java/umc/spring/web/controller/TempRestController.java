8298d78b17f6c7d0bd69157407dea98d410a6c72
tellgeniewish <jadastras2@gmail.com>
2025-05-11 03:25:34 +0900

    :sparkles: practice finish

--- week07/practice/spring/spring/src/main/java/umc/spring/web/controller/TempRestController.java
+++ week07/practice/spring/spring/src/main/java/umc/spring/web/controller/TempRestController.java
@@ -3,19 +3,34 @@ package umc.spring.web.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import umc.spring.apiPayload.ApiResponse;
 import umc.spring.converter.TempConverter;
+import umc.spring.service.TempService.TempQueryService;
 import umc.spring.web.dto.TempResponse;
 
 @RestController
 @RequestMapping("/temp")
 @RequiredArgsConstructor
 public class TempRestController {
+    private final TempQueryService tempQueryService;
 
     @GetMapping("/test")
     public ApiResponse<TempResponse.TempTestDTO> testAPI(){
 
         return ApiResponse.onSuccess(TempConverter.toTempTestDTO());
+    }
+//    @GetMapping("/test")
+//    public ResponseDto<TempResponse.TempTestDTO> testAPI(){
+//
+//        return ResponseDto.onSuccess(TempConverter.toTempTestDTO(), Code.OK);
+//    }
+
+    @GetMapping("/exception")
+    public ApiResponse<TempResponse.TempExceptionDTO> exceptionAPI(@RequestParam Integer flag){
+        tempQueryService.CheckFlag(flag);
+        return ApiResponse.onSuccess(TempConverter.toTempExceptionDTO(flag));
+//        return null;
     }
 }
