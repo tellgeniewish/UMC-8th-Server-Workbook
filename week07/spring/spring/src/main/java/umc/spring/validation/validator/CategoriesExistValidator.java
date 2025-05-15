79fd44bd2fe7996c8e9c4e661dc59ac4aa1d3253
tellgeniewish <jadastras2@gmail.com>
2025-05-16 07:04:18 +0900

    :sparkles: practice finish

--- week07/spring/spring/src/main/java/umc/spring/validation/validator/CategoriesExistValidator.java
+++ week07/spring/spring/src/main/java/umc/spring/validation/validator/CategoriesExistValidator.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import umc.spring.apiPayload.code.status.ErrorStatus;
 import umc.spring.repository.FoodCategoryRepository.FoodCategoryRepository;
+import umc.spring.service.FoodCategoryService.FoodCategoryService;
 import umc.spring.validation.annotation.ExistCategories;
 
 import java.util.List;
@@ -14,7 +15,8 @@ import java.util.List;
 @RequiredArgsConstructor
 public class CategoriesExistValidator implements ConstraintValidator<ExistCategories, List<Long>> {
 
-    private final FoodCategoryRepository foodCategoryRepository;
+//    private final FoodCategoryRepository foodCategoryRepository;
+    private final FoodCategoryService foodCategoryService;
 
     @Override
     public void initialize(ExistCategories constraintAnnotation) {
@@ -23,8 +25,9 @@ public class CategoriesExistValidator implements ConstraintValidator<ExistCatego
 
     @Override
     public boolean isValid(List<Long> values, ConstraintValidatorContext context) {
-        boolean isValid = values.stream()
-                .allMatch(value -> foodCategoryRepository.existsById(value));
+//        boolean isValid = values.stream()
+//                .allMatch(value -> foodCategoryRepository.existsById(value));
+        boolean isValid = foodCategoryService.doAllCategoriesExist(values);
 
         if (!isValid) {
             context.disableDefaultConstraintViolation();
