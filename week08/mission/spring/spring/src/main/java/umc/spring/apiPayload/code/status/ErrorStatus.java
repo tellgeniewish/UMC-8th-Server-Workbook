b0e817190abfd382ba15cb56586f35efec7cbecf
tellgeniewish <jadastras2@gmail.com>
2025-05-18 22:59:13 +0900

    :sparkles: 특정 지역에 가게 추가하기 API 완료

--- week08/mission/spring/spring/src/main/java/umc/spring/apiPayload/code/status/ErrorStatus.java
+++ week08/mission/spring/spring/src/main/java/umc/spring/apiPayload/code/status/ErrorStatus.java
@@ -26,9 +26,9 @@ public enum ErrorStatus implements BaseErrorCode {
     // For test
     TEMP_EXCEPTION(HttpStatus.BAD_REQUEST, "TEMP4001", "이거는 테스트"),
 
-    FOOD_CATEGORY_NOT_FOUND(HttpStatus.BAD_REQUEST, "FOODCATE4001", "음식 카테고리 없습니다."),
+    FOOD_CATEGORY_NOT_FOUND(HttpStatus.NOT_FOUND, "FOODCATE4001", "음식 카테고리 없습니다."),
 
-    REGION_NOT_FOUND(HttpStatus.BAD_REQUEST, "REGION4001", "해당 지역이 존재하지 않습니다.");
+    REGION_NOT_FOUND(HttpStatus.NOT_FOUND, "REGION4001", "해당 지역이 존재하지 않습니다.");
 
     private final HttpStatus httpStatus;
     private final String code;
