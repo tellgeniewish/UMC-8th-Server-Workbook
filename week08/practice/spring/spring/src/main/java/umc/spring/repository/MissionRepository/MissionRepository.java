a7756d1ab3e18934c7b1e551767de236662e7da6
tellgeniewish <jadastras2@gmail.com>
2025-05-19 16:15:37 +0900

    only week08 mission

--- week08/practice/spring/spring/src/main/java/umc/spring/repository/MissionRepository/MissionRepository.java
+++
@@ -1,7 +0,0 @@
-package umc.spring.repository.MissionRepository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import umc.spring.domain.Mission;
-
-public interface MissionRepository extends JpaRepository<Mission, Long>, MissionRepositoryCustom {
-}
--- week08/practice/spring/spring/src/main/java/umc/spring/repository/ReviewRepository/ReviewRepository.java
+++
@@ -1,7 +0,0 @@
-package umc.spring.repository.ReviewRepository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import umc.spring.domain.Review;
-
-public interface ReviewRepository extends JpaRepository<Review, Long>, ReviewRepositoryCustom {
-}
