b3a66f486a810be330fca999c8db51ddae7ac043
tellgeniewish <jadastras2@gmail.com>
2025-05-27 03:16:35 +0900

    :sparkles: 서비스 코드 작성

--- week09/practice/spring/spring/src/main/java/umc/spring/service/StoreService/StoreQueryService.java
+++ week09/practice/spring/spring/src/main/java/umc/spring/service/StoreService/StoreQueryService.java
@@ -1,5 +1,7 @@
 package umc.spring.service.StoreService;
 
+import org.springframework.data.domain.Page;
+import umc.spring.domain.Review;
 import umc.spring.domain.Store;
 import java.util.List;
 import java.util.Optional;
@@ -8,4 +10,5 @@ public interface StoreQueryService {
 
     Optional<Store> findStore(Long id);
     List<Store> findStoresByNameAndScore(String name, Float score);
+    Page<Review> getReviewList(Long StoreId, Integer page);
 }
