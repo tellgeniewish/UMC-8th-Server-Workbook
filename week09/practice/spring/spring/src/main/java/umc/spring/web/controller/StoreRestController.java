6df4346a1d0980ff2e95fc49817f7b2dde9b7b1f
tellgeniewish <jadastras2@gmail.com>
2025-05-27 08:38:35 +0900

    :refactor: 특정 가게 미션 목록 조회 컨트롤러 작성

--- week09/practice/spring/spring/src/main/java/umc/spring/web/controller/StoreRestController.java
+++ week09/practice/spring/spring/src/main/java/umc/spring/web/controller/StoreRestController.java
@@ -11,13 +11,17 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.*;
 import umc.spring.apiPayload.ApiResponse;
+import umc.spring.converter.MissionConverter;
 import umc.spring.converter.ReviewConverter;
 import umc.spring.converter.StoreConverter;
+import umc.spring.domain.Mission;
 import umc.spring.domain.Review;
 import umc.spring.domain.Store;
 import umc.spring.service.StoreService.StoreCommandService;
 import umc.spring.service.StoreService.StoreQueryService;
 import umc.spring.validation.annotation.ExistStore;
+import umc.spring.validation.annotation.ValidPage;
+import umc.spring.web.dto.MissionResponseDTO;
 import umc.spring.web.dto.ReviewResponseDTO;
 import umc.spring.web.dto.StoreRequestDTO;
 import umc.spring.web.dto.StoreResponseDTO;
@@ -53,5 +57,21 @@ public class StoreRestController {
         Page<Review> reviewList = storeQueryService.getReviewList(storeId,page);
         return ApiResponse.onSuccess(ReviewConverter.reviewPreViewListDTO(reviewList));
 //        return null;
+    }
+
+    @GetMapping("/{storeId}/missions")
+    @Operation(summary = "특정 가게의 미션 목록 조회 API", description = "특정 가게의 미션 목록을 조회하는 API입니다. 페이징을 포함하니 query string으로 page 번호를 함께 보내주세요.")
+    @ApiResponses({
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "COMMON200", description = "OK, 성공"),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "AUTH003", description = "access 토큰을 주세요!",content = @Content(schema = @Schema(implementation = ApiResponse.class))),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "AUTH004", description = "access 토큰 만료",content = @Content(schema = @Schema(implementation = ApiResponse.class))),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "AUTH006", description = "access 토큰 모양이 이상함",content = @Content(schema = @Schema(implementation = ApiResponse.class))),
+    })
+    @Parameters({
+            @Parameter(name = "storeId", description = "가게의 아이디. path variable입니다.")
+    })
+    public ApiResponse<MissionResponseDTO.MissionPreViewListDTO> getMissionList(@ExistStore @PathVariable(name = "storeId") Long storeId, @ValidPage @RequestParam(name = "page", defaultValue = "1") Integer page) {
+        Page<Mission> missionList = storeQueryService.getMissionList(storeId,page - 1);
+        return ApiResponse.onSuccess(MissionConverter.missionPreviewListDTO(missionList));
     }
 }
