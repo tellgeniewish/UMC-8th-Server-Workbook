cc11663eeff2299bfe17b3a740c23caeec9b9aac
tellgeniewish <jadastras2@gmail.com>
2025-05-27 07:23:49 +0900

    :sparkles: 내가 작성한 리뷰 목록

--- week09/practice/spring/spring/src/main/java/umc/spring/web/controller/MemberRestController.java
+++ week09/practice/spring/spring/src/main/java/umc/spring/web/controller/MemberRestController.java
@@ -1,17 +1,27 @@
 package umc.spring.web.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.web.bind.annotation.*;
 import umc.spring.apiPayload.ApiResponse;
 import umc.spring.converter.MemberConverter;
+import umc.spring.converter.ReviewConverter;
 import umc.spring.domain.Member;
+import umc.spring.domain.Review;
 import umc.spring.service.MemberService.MemberCommandService;
+import umc.spring.service.MemberService.MemberQueryService;
+import umc.spring.service.ReviewService.ReviewQueryService;
+import umc.spring.validation.annotation.ExistStore;
 import umc.spring.web.dto.MemberRequestDTO;
 import umc.spring.web.dto.MemberResponseDTO;
+import umc.spring.web.dto.ReviewResponseDTO;
 
 @RestController
 @RequiredArgsConstructor
@@ -19,11 +29,29 @@ import umc.spring.web.dto.MemberResponseDTO;
 public class MemberRestController {
 
     private final MemberCommandService memberCommandService;
+    private final MemberQueryService memberQueryService;
 
     @PostMapping("/")
     public ApiResponse<MemberResponseDTO.JoinResultDTO> join(@RequestBody @Valid MemberRequestDTO.JoinDto request){
         Member member = memberCommandService.joinMember(request);
         return ApiResponse.onSuccess(MemberConverter.toJoinResultDTO(member));
 //        return null
+    }
+
+    @GetMapping("/{memberId}/reviews")
+    @Operation(summary = "내가 작성한 리뷰 목록 조회 API",description = "내가 작성한 리뷰들의 목록을 조회하는 API이며, 페이징을 포함합니다. query String 으로 page 번호를 주세요")
+    @ApiResponses({
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "COMMON200",description = "OK, 성공"),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "AUTH003", description = "access 토큰을 주세요!",content = @Content(schema = @Schema(implementation = ApiResponse.class))),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "AUTH004", description = "acess 토큰 만료",content = @Content(schema = @Schema(implementation = ApiResponse.class))),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "AUTH006", description = "acess 토큰 모양이 이상함",content = @Content(schema = @Schema(implementation = ApiResponse.class))),
+    })
+    @Parameters({
+            @Parameter(name = "memberId", description = "가게의 아이디, path variable 입니다!")
+    })
+    public ApiResponse<ReviewResponseDTO.ReviewPreViewListDTO> getMemberReviewList(@PathVariable(name = "memberId") Long memberId, @RequestParam(name = "page") Integer page){
+        Page<Review> reviewList = memberQueryService.getMemberReviewList(memberId,page);
+        return ApiResponse.onSuccess(ReviewConverter.reviewPreViewListDTO(reviewList));
+//        return null;
     }
 }
