e096bbeba3086242f77a9a4a500a9b7bfc8ebe5c
tellgeniewish <jadastras2@gmail.com>
2025-05-27 08:27:40 +0900

    :sparkles: 특정 가게 미션 목록 조회 DTO 작성

--- week09/practice/spring/spring/src/main/java/umc/spring/web/dto/MissionResponseDTO.java
+++ week09/practice/spring/spring/src/main/java/umc/spring/web/dto/MissionResponseDTO.java
@@ -5,7 +5,9 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.List;
 
 public class MissionResponseDTO {
     @Builder
@@ -15,5 +17,29 @@ public class MissionResponseDTO {
     public static class AddMissionResultDTO{
         Long missionId;
         LocalDateTime createdAt;
+    }
+
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class MissionPreViewListDTO {
+        List<MissionResponseDTO.MissionPreViewDTO> missionList;
+        Integer listSize;
+        Integer totalPage;
+        Long totalElements;
+        Boolean isFirst;
+        Boolean isLast;
+    }
+
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class MissionPreViewDTO {
+        Long missionId;
+        String missionSpec;
+        Integer reward;
+        LocalDate deadline;
     }
 }
