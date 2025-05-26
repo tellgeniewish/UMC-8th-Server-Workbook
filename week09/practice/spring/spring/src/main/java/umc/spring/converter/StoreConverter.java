9449ae01511dceac9b7fb8c753dc540a0e5b1d9e
tellgeniewish <jadastras2@gmail.com>
2025-05-27 01:58:12 +0900

    :sparkles: converter 작성

--- week09/practice/spring/spring/src/main/java/umc/spring/converter/StoreConverter.java
+++ week09/practice/spring/spring/src/main/java/umc/spring/converter/StoreConverter.java
@@ -1,11 +1,13 @@
 package umc.spring.converter;
 
 import umc.spring.domain.Region;
+import umc.spring.domain.Review;
 import umc.spring.domain.Store;
 import umc.spring.web.dto.StoreRequestDTO;
 import umc.spring.web.dto.StoreResponseDTO;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 public class StoreConverter {
     public static StoreResponseDTO.AddStoreResultDTO toAddStoreResultDTO(Store store){
@@ -22,5 +24,12 @@ public class StoreConverter {
                 .score(request.getScore())
                 .region(region)
                 .build();
+    }
+
+    public static StoreResponseDTO.ReviewPreViewDTO reviewPreViewDTO(Review review){
+        return null;
+    }
+    public static StoreResponseDTO.ReviewPreViewListDTO reviewPreViewListDTO(List<Review> reviewList){
+        return null;
     }
 }
