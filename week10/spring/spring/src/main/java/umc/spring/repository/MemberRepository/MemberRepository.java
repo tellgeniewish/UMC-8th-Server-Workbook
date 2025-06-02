dc927807a6879250700c096af541a22017e65804
tellgeniewish <jadastras2@gmail.com>
2025-06-02 23:50:15 +0900

    :sparkles: 로그인 구현

--- week10/spring/spring/src/main/java/umc/spring/repository/MemberRepository/MemberRepository.java
+++ week10/spring/spring/src/main/java/umc/spring/repository/MemberRepository/MemberRepository.java
@@ -3,5 +3,8 @@ package umc.spring.repository.MemberRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import umc.spring.domain.Member;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Long>, umc.spring.repository.MemberRepository.MemberRepositoryCustom {
+    Optional<Member> findByEmail(String email);
 }
